e3344603a0a58fbecf76fd92058ff6b8cc6d8623
Abhay Singh <246abhaysingh@gmail.com>
2024-09-17 05:37:18 -0700

    object created

---
+++ bcard/src/components/data.js
@@ -0,0 +1,18 @@
+export default {
+    id: 1,
+    name: "Abhay Singh",
+    numb: "310-467-6794",
+    email: "246abhaysingh@gmail.com",
+    title: "Full Stack Developer",
+    About: `I am currently a 4th year student pursuing a B.S. in Computer
+            Science and Minor in Informatics at the University of
+            California, Irvine. I transferred from El Camino College, where
+            I earned a Associates in Mathematics. Computer Science is a passion
+            of mine because it requires me to constantly learn and adapt to create
+            new and effective solutions to problems.`,
+    Interests: `Outside of coding, I love to play games, workout, try new foods, 
+                and pick up new skills. I love to play basketball, football, golf,
+                table tennis, and tennis. I enjoy meeting new people and finding
+                new hobbies`
+
+}
